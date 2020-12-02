8264b8669fde548de060b3f4446803a6ad10c372
Neil Lambert <nlambert@doximity.com>
2020-12-02 01:15:21 -0800

    Day1, Part1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # advent-of-code-2020
+
+| Link | Code |
+|------|------|
+|[Day 1](https://adventofcode.com/2020/day/1) | [Code](days/day_1/solution.rb)|
+|[Day 2](https://adventofcode.com/2020/day/2) | [Code](days/day_2/solution.rb)|
--- days/day_1/solution.rb
+++ days/day_1/solution.rb
@@ -22,3 +22,21 @@
 # In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 
 # Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+#
+
+expenses = File.open("part_1.input.txt").map(&:to_i)
+sums = {}
+expenses.each { |expense| sums.store(2020 - expense, expense) }
+puts expenses.each { |expense| break(expense * sums[expense]) if sums.has_key? expense }
+
+###
+# --- Part Two ---
+# The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+# Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+# In your expense report, what is the product of the three entries that sum to 2020?
+# ###
+
+
+# grr, hash trick aint gonna work
